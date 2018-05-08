aac4ddf1245169bdfcc17bec0d717475ae8778b5
Suguru Tanaka <sss.329@icloud.com>
2018-05-08 10:01:14 +0900

    watch test

--- cri_calender/src/com/cri/watch/WatchMainFrame.java
+++ cri_calender/src/com/cri/watch/WatchMainFrame.java
@@ -1,8 +1,18 @@
 package com.cri.watch;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class WatchMainFrame {
 	public static void main (String args[]) {
-		System.out.println("hello");
+		Date d = new Date();
+
+		SimpleDateFormat d1 = new SimpleDateFormat("yyyy/MM/dd");
+		String c1 = d1.format(d);
+		System.out.println(c1);
+
+		SimpleDateFormat d2 = new SimpleDateFormat("hh:mm");
+		String c2 = d2.format(d);
+		System.out.println(c2);
 	}
 
 
