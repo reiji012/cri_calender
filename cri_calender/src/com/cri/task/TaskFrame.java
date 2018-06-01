f88a9ae17e4bfa123c93746a3fb872df6aa2e8b8
Furuno Shuji <south24men@gmail.com>
2018-06-01 23:49:01 +0900

    TaskMainFrameに沿ってコード追加

--- cri_calender/src/com/cri/task/TaskFrame.java
+++ cri_calender/src/com/cri/task/TaskFrame.java
@@ -2,17 +2,28 @@ package com.cri.task;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 
 public class TaskFrame extends JFrame {
 
 	private JPanel contentPane;
+	private JPanel head;
+	private JLabel date;
+	private JButton addButton;
+	private JScrollPane scrollPane;
+	private JTextArea textArea;
 
 	/**
 	 * Launch the application.
@@ -38,26 +49,58 @@ public class TaskFrame extends JFrame {
 	public TaskFrame(){
 		setForeground(Color.GRAY);
 		setType(Type.UTILITY);
+		setBackground(Color.GRAY);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(475,220);
+		setSize(450,300);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.GRAY);
+		contentPane.setPreferredSize(new Dimension(475, 220));
 		contentPane.setBorder(new EmptyBorder(5,5,5,5));
 		contentPane.setLayout(new BorderLayout(0,0));
 		contentPane.setBackground(Color.GRAY);
 		setContentPane(contentPane);
 
 
-		JPanel head = new JPanel();
+		head = new JPanel();
 		head.setLayout(new BorderLayout(0,0));
 		contentPane.add(head, BorderLayout.NORTH);
 
 		//日付を追加する
-		JLabel date = new JLabel("日付");
+		date = new JLabel("日付");
 		head.add(date, BorderLayout.WEST);
+		head.setBackground(Color.GRAY);
 
 		//ボタンを追加する
-		JButton btn1 = new JButton("+");
-		head.add(btn1, BorderLayout.EAST);
+		addButton = new JButton("+");
+		head.add(addButton, BorderLayout.EAST);
+		addButton.setBackground(Color.GRAY);
+
+		//スクロールパネルを追加する
+		scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		setBackground(Color.GRAY);
+
+		//テキストエリアを追加する
+		textArea = new JTextArea();
+		textArea.setText("タスクはありません");
+		textArea.setEditable(false);
+		scrollPane.setViewportView(textArea);
+		textArea.setBackground(Color.GRAY);
+
+		//マウスをクリックした時
+		addButton.addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+				DialogWindow dlg = new DialogWindow(null);
+				getContentPane().setLayout(new FlowLayout());
+
+				dlg.actionPerformed(null);
+
+			}
+		});
+
+
+
+
 
 
 	}
