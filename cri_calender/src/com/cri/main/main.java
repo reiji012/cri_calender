7f3138b9da487aaf226b0d57c86d731600b6c77e
川尻千里 <>
2018-07-27 20:01:17 +0900

    calendarBaseDevelop

--- cri_calender/src/com/cri/main/main.java
+++ cri_calender/src/com/cri/main/main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("good afternoon");
-		System.out.println("hello");
+		System.out.println("hello Sasaki");
 	}
 
 }
